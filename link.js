6e34971a2b64fc2e58961527024669d8e1b2aa85
Leland Richardson <leland.m.richardson@gmail.com>
2016-01-22 20:23:17 -0800

    Initial Commit

---
+++ link.js
@@ -0,0 +1,6 @@
+javascript:(function(){
+  var s = document.createElement('SCRIPT');
+  s.type='text/javascript';
+  s.src='https://rawgit.com/lelandrichardson/6e2ff8bb24b35cc9d17b/raw/b7966ee6c2572d79779c3cbb0383e13be46e7c76/bookmarklet.all.js';
+  document.getElementsByTagName('head')[0].appendChild(s);
+})();
